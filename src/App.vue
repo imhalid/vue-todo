21ea05116297683d0503c314508ff53c7651d731
imhalid <imhalid@icloud.com>
2023-03-13 15:42:47 +0300

    delete item

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,41 @@
 <template>
   <div
-    class="h-[650px] w-96 bg-neutral-200 rounded-3xl shadow-lg border-2 px-5 border-gray-400"
+    class="h-[650px] w-96 bg-neutral-200 rounded-3xl shadow-lg border-2 px-5 border-gray-400 overflow-hidden"
   >
     <Title :title="title" />
     <div>
       <input
         type="text"
-        class="w-full flex h-10 px-3"
+        class="w-full flex h-10 px-3 rounded-md outline-none focus:ring-0"
         placeholder="add item"
         @keyup.enter="addData"
+        :value="data.title"
       />
     </div>
-    <div
-      v-for="(item, index) in data"
-      :key="index"
-      class="w-auto h-20 flex place-items-center justify-between bg-neutral-300 mt-5 rounded-lg"
-    >
-      <div class="mx-3 flex items-center">
-        <button @click="toggleComplete(item)">
-          <div class="w-5 h-5 rounded-full border-2 border-black relative">
-            <div
-              class="bg-gradient-to-tr from-emerald-500 to bg-emerald-600 w-3 h-3 transition-all rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-              :class="{ 'scale-0': !item.complete }"
-            ></div>
-          </div>
-        </button>
-      </div>
-      <div class="flex-1 text-left text-neutral-900">
-        {{ item.id }}
-      </div>
-      <div class="aspect-square h-10 flex items-center justify-center">
-        <button>
-          <DeleteIcon class="h-7 w-7 text-rose-500" />
-        </button>
+    <div class="h-full overflow-auto scroll-smooth">
+      <div
+        v-for="(item, index) in data"
+        :key="index"
+        class="w-auto h-20 flex place-items-center justify-between bg-neutral-300 mt-5 rounded-lg"
+      >
+        <div class="mx-3 flex items-center">
+          <button @click="toggleComplete(item)">
+            <div class="w-5 h-5 rounded-full border-2 border-black relative">
+              <div
+                class="bg-gradient-to-tr from-emerald-500 to bg-emerald-600 w-3 h-3 transition-all rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                :class="{ 'scale-0': !item.complete }"
+              ></div>
+            </div>
+          </button>
+        </div>
+        <div class="flex-1 text-left text-neutral-900">
+          {{ item.id }}
+        </div>
+        <div class="aspect-square h-10 flex items-center justify-center">
+          <button @click="deleteData">
+            <DeleteIcon class="h-7 w-7 text-rose-500" />
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -43,20 +46,7 @@ import DeleteIcon from '@/icons/DeleteIcon.vue'
 import { ref } from 'vue'
 import Title from './components/Title.vue'
 
-let complete = ref(false)
-
-const data = ref([
-  {
-    id: 5128517298,
-    title: 'Hello World',
-    complete: false,
-  },
-  {
-    id: 125125,
-    title: 'Hello World',
-    complete: false,
-  },
-])
+const data = ref([])
 const title = 'Content'
 
 const toggleComplete = e => {
@@ -65,9 +55,12 @@ const toggleComplete = e => {
 
 const addData = () => {
   data.value.push({
-    id: 5128517298,
+    id: new Date().getTime(),
     title: 'Hello World',
     complete: false,
   })
+}
+const deleteData = event => {
+  data.value.splice(event, 1)
 }
 </script>
